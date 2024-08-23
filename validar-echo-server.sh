c5b2eab47dc6c8978b125a719fff280bf8c67595
Merok23 <alejocalizblanco@gmail.com>
2024-08-23 15:01:44 -0300

    Add the resolution to excercise 3
    Create a script for testing the echo server.
    Modify dockerfiles to use CMD instead of ENTRYPOINT since this created problems with docker run and binaries.

---
+++ validar-echo-server.sh
@@ -0,0 +1,12 @@
+docker network create tp0_net
+docker network connect tp0_net server
+input="hola"
+response=$(echo "$input" | docker run --network tp0_net -i client:latest sh -c 'nc server 12345')
+echo "Response: $response"
+if [ "$input" == "$response" ]; then
+  echo "action: test_echo_server | result: success"
+else
+  echo "action: test_echo_server | result: fail"
+fi
+docker network disconnect tp0_net server
+docker network rm tp0_net
