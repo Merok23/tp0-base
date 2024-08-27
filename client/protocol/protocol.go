3907def11f3d034f16cd30d64d5a86a37ab4de6b
Merok23 <alejocalizblanco@gmail.com>
2024-08-27 11:52:51 -0300

    feat: Add a code before sending a message for command recognition.

--- client/protocol/protocol.go
+++ client/protocol/protocol.go
@@ -10,12 +10,21 @@ import (
 
 var log = logging.MustGetLogger("log")
 
+func encodeInt32(value int) []byte {
+	bytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(bytes, uint32(value))
+	return bytes
+}
+
 func SendMsg(conn net.Conn,  id int, msgID int) error {
-	// Send the size (4 bytes)
+	// Send the code
+	codeBytes := encodeInt32(1)
+	conn.Write(codeBytes)
+	log.Infof("action: send_code | result: success | client_id: %v | code: 1", id)
+	// Send the size
 	msg := fmt.Sprintf("[CLIENT %v] Message N°%v\n", id, msgID)
 	size := len(msg)
-	sizeBytes := make([]byte, 4)
-	binary.BigEndian.PutUint32(sizeBytes, uint32(size))
+	sizeBytes := encodeInt32(size)
 	conn.Write(sizeBytes)
 	log.Infof("action: send_size | result: success | client_id: %v | size: %v", id, size)
 
--- server/common/protocol.py
+++ server/common/protocol.py
@@ -6,6 +6,9 @@ import socket
 import logging
 from common.socket_tcp import SocketTCP
 
+CODE_SIZE = 4
+SIZE_SIZE = 4
+
 class Protocol:
     """
     Class that abstracts the protocol
@@ -15,7 +18,37 @@ class Protocol:
         """
         Receive message from the client (and handle short reads)
         """
-        size_bytes = client_sock.recv(4) # TODO: Formalize protocol.
-        size = int.from_bytes(size_bytes, byteorder='big') # network to host
+        code = Protocol.__receive_message_code(client_sock)
+        if code == 1:
+            logging.info("action: receive_client_message | result: success | code: %d", code)
+            return Protocol.__receive_message(client_sock)
+        raise ValueError("Invalid code")
+
+    @staticmethod
+    def __receive_message(client_sock: socket) -> str:
+        """
+        Receive message from the client
+        """
+        size = Protocol.__receive_message_size(client_sock)
         logging.info("action: receive_message | result: in_progress | size: %d", size)
         return SocketTCP.receive_all(client_sock, size).decode('utf-8')
+
+    @staticmethod
+    def __receive_message_code(client_sock: socket) -> int:
+        """
+        Receive message code from the client
+        """
+        code = client_sock.recv(CODE_SIZE)
+        code = int.from_bytes(code, byteorder='big')
+        logging.info("action: receive_message_code | result: success | code: %d", code)
+        return code
+
+    @staticmethod
+    def __receive_message_size(client_sock: socket) -> int:
+        """
+        Receive message size from the client
+        """
+        size_bytes = client_sock.recv(SIZE_SIZE)
+        size = int.from_bytes(size_bytes, byteorder='big')
+        logging.info("action: receive_message_size | result: success | size: %d", size)
+        return size
